8407fe1d5a7bb181945a859fde565e1f60fe26a2
VladislavBurch <31613623+VladislavBurch@users.noreply.github.com>
2017-12-21 16:20:46 +0300

    Update message_tests.py

--- tests/message_tests.py
+++ tests/message_tests.py
@@ -3,5 +3,8 @@
 import main
 
 class testListBot:
-    def simple_test():
+    def simple_mes():
         main.bot.send_message(203344707, "HIhi")
+        return 0
+    def test_pos():
+        assert simple_mes() == 0
