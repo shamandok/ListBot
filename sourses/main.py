12726ac3cd914aaefbaf38565cff596acb429333
VladislavBurch <31613623+VladislavBurch@users.noreply.github.com>
2017-12-21 13:59:21 +0300

    Update main.py

--- sourses/main.py
+++ sourses/main.py
@@ -1,9 +1,10 @@
 import telebot
+import constants
 
-bot = telebot.TeleBot("TOKEN")
+bot = telebot.TeleBot(constants.key)
 
 all_updates = bot.get_updates()
-last_update = all_updates[-1]
+last_update = all_updates[len(all_updates)-1]
 
 
 def log(message, answer):
@@ -19,82 +20,63 @@ def log(message, answer):
 
 @bot.message_handler(commands=['start'])
 def handle_text(message):
-    bot.send_message(message.chat.id, """Hi, I am timetable bot!
-
-You can see the list of commands using /list
-or you can send me the day
-of the week to see the timetable for this""")
+    bot.send_message(message.chat.id, constants.start_message)
 
 
 @bot.message_handler(commands=['list'])
 def handle_text(message):
-        bot.send_message(message.chat.id, """You can use /start to start the dialog
-
-You can use /list to see all commands
-You can use /help to read more about the bot""")
+        bot.send_message(message.chat.id, constants.list_message)
 
 
 @bot.message_handler(commands=['help'])
 def handle_text(message):
-        keyboard = telebot.types.InlineKeyboardMarkup()
-        url_button = telebot.types.InlineKeyboardButton(text="Go to GitHub", url="https://students.bmstu.ru/schedule/62f5611c-a264-11e5-b4d3-005056960017")
-        keyboard.add(url_button)
-        bot.send_message(message.chat.id, "Посмотреть исходный код", reply_markup=keyboard)
+    keyboard = telebot.types.InlineKeyboardMarkup()
+    url_button = telebot.types.InlineKeyboardButton(text="Перейти на GitHub",
+                                                    url=constants.github_url)
+    keyboard.add(url_button)
+    bot.send_message(message.chat.id, "Исходный код", reply_markup=keyboard)
+
+
+@bot.message_handler(commands=['site'])
+def handle_text(message):
+    keyboard = telebot.types.InlineKeyboardMarkup()
+    url_button = telebot.types.InlineKeyboardButton(text="Перейти на сайт МГТУ",
+                                                    url=constants.bmstu_url)
+    keyboard.add(url_button)
+    bot.send_message(message.chat.id, "Нажми на кнопку и посмотри расписание на сайте МГТУ", reply_markup=keyboard)
 
 
 @bot.message_handler(content_types=['text'])
 def handle_text(message):
     if (message.text == 'Hi') or (message.text == 'Hello'):
-        answer = 'Just send me the day of the week'
+        answer = constants.content[0]
         bot.send_message(message.chat.id, answer)
-        log(message,answer)
-    elif (message.text == 'Monday') or message.text == 'monday' or message.text == 'mon' or message.text == 'Mon':
-        answer = """10:15 - 11:50   АЯП (лаб)
-12:00 - 13:35  АЯП (сем)
-13:50 - 15:25  Дискретная математика (сем)
-15:40 - 17:15   Иностр. язык
-17:35 - 19:00  Экология (лек)"""
+        log(message, answer)
+    elif (message.text == 'Monday') or message.text == 'monday' or message.text == 'mon' or message.text == 'Mon' or message.text == 'Понедельник':
+        answer = constants.content[1]
         bot.send_message(message.chat.id, answer)
         log(message, answer)
     elif (message.text == 'Tuesday') or message.text == 'tuesday' or message.text == 'tue' or message.text == 'Tue':
-        answer = """12:00 - 13:35  Физика(лаб/--)
-13:50 - 15:25  Физика(лаб/сем)
-15:40 - 17:15   Физика (лекция)
-17:35 - 19:00  АЯП (лек/--)"""
+        answer = constants.content[2]
         bot.send_message(message.chat.id, answer)
         log(message, answer)
     elif (message.text == 'Wednesday') or message.text == 'wednesday' or message.text == 'wed' or message.text == 'Wed':
-        answer = """8:30 - 10:05  Электротехника (лек)
-10:15 - 11:50  Методы оптимизации (лек)
-12:00 - 13:25  Физкультура"""
+        answer = constants.content[3]
         bot.send_message(message.chat.id, answer)
         log(message, answer)
 
     elif (message.text == 'Thursday') or message.text == 'thursday' or message.text == 'thu' or message.text == 'Thu':
-        answer = """8:30 - 10:05  Политология (сем)
-10:15 - 11:50  Дискретная математика (лекция)
-12:00 - 13:25  Политология (лек/--)"""
+        answer = constants.content[4]
         bot.send_message(message.chat.id, answer)
         log(message, answer)
 
     elif (message.text == 'Friday') or message.text == 'friday' or message.text == 'fri' or message.text == 'Fri':
-        answer = """8:30 - 10:05  Электротехника (сем)
-10:15 - 11:50  ----
-12:00 - 13:25  Электротехника (лаб)
-13:35 - 15:25  Электротехника (лаб)"""
+        answer = constants.content[5]
         bot.send_message(message.chat.id, answer)
         log(message, answer)
 
-    elif (message.text == 'Сайт'):
-        keyboard = telebot.types.InlineKeyboardMarkup()
-        url_button = telebot.types.InlineKeyboardButton(text="Перейти на сайт МГТУ", url="https://students.bmstu.ru/schedule/62f5611c-a264-11e5-b4d3-005056960017")
-        keyboard.add(url_button)
-        bot.send_message(message.chat.id, "Нажми на кнопку и посмотри расписание на сайте МГТУ", reply_markup=keyboard)
-
-
     else:
-        answer = """I do not know what you mean
-Use /list to see all commands"""
+        answer = constants.unknown_text
         bot.send_message(message.chat.id, answer)
         log(message, answer)
 
