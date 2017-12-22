8e7eec58c25e6140bcda5c840ef7dc2b559dcbbd
VladislavBurch <31613623+VladislavBurch@users.noreply.github.com>
2017-12-23 01:28:44 +0300

    Update message_tests.py

--- tests/message_tests.py
+++ tests/message_tests.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 
-import main
-
 class testListBot:
+    bot=telebot.Tlebot("458178330:AAFU4pElGPQb06VbUzypJzHtdzH107Ngqoc")
     def simple_mes():
-        main.bot.send_message(203344707, "HIhi")
+        bot.send_message(203344707, "HiHi!")
         return 0
     def test_pos():
         assert simple_mes() == 0
