fbffd0ec0897730c1272011fd7ee5910c7fb4006
VladislavBurch <31613623+VladislavBurch@users.noreply.github.com>
2017-12-23 06:00:00 +0300

    Update message_tests.py

--- tests/message_tests.py
+++ tests/message_tests.py
@@ -1,5 +1,27 @@
-# -*- coding: utf-8 -*-
-class test:
-    def sum(a,b):
-        return a+b
-    assert sum(1,3) == 4
+import unittest
+import telebot
+import os
+
+
+class TestStringMethods(unittest.TestCase):
+    @unittest.skipIf('TOKEN' and 'CHAT_ID' not in os.environ, "No environment variables configured")
+    def test_send_mes(self):
+        bot = telebot.TeleBot(os.environ['TOKEN'])
+        ret_msg = bot.send_message(os.environ['CHAT_ID'], "text")
+        assert ret_msg.message_id
+
+
+    @unittest.skipIf('TOKEN' and 'CHAT_ID' not in os.environ, "No environment variables configured")
+    def test_send_message_with_markdown(self):
+        bot = telebot.TeleBot(os.environ['TOKEN'])
+        markdown = """
+           *bold text*
+           _italic text_
+           [text](URL)
+           """
+        ret_msg = bot.send_message(os.environ['CHAT_ID'], markdown, parse_mode="Markdown")
+        assert ret_msg.message_id
+
+
+if __name__ == '__main__':
+    unittest.main()
