6f0f6d8a138e2c3b3991283670c8a3b1d6bffe2b
VladislavBurch <31613623+VladislavBurch@users.noreply.github.com>
2018-01-02 22:00:10 +0300

    Update constants.py

--- import/constants.py
+++ import/constants.py
@@ -1,36 +1,10 @@
+start_mes = """Привет, меня зовут ListBot. Для начала, я бы хотел спросить: 
+Нужно ли тебе загружать расписание группы ИУ8-33, или ты будешь пользоваться мной и без этого?"""
 
-key = "default key"
+help_mes = """Some helpful info"""
 
-start_message = """Hi, I am timetable bot!
-
-You can see the list of commands using /list
-or you can send me the day
-of the week to see the timetable for this"""
-
-list_message = """You can use /start to start the dialog
-
-You can use /list to see all commands
-You can use /help to read more about the bot"""
-
-content = ['Just send me the day of the week', """10:15 - 11:50   АЯП (лаб)
-12:00 - 13:35  АЯП (сем)
-13:50 - 15:25  Дискретная математика (сем)
-15:40 - 17:15   Иностр. язык
-17:35 - 19:00  Экология (лек)""", """12:00 - 13:35  Физика(лаб/--)
-13:50 - 15:25  Физика(лаб/сем)
-15:40 - 17:15   Физика (лекция)
-17:35 - 19:00  АЯП (лек/--)""", """8:30 - 10:05  Электротехника (лек)
-10:15 - 11:50  Методы оптимизации (лек)
-12:00 - 13:25  Физкультура""", """8:30 - 10:05  Политология (сем)
-10:15 - 11:50  Дискретная математика (лекция)
-12:00 - 13:25  Политология (лек/--)""", """8:30 - 10:05  Электротехника (сем)
-10:15 - 11:50  ----
-12:00 - 13:25  Электротехника (лаб)
-13:35 - 15:25  Электротехника (лаб)"""]
-
-unknown_text = """I do not know what you mean
-Use /list to see all commands"""
-
-github_url = "https://github.com/vladislavburch/ListBot"
-
-bmstu_url = "https://students.bmstu.ru/schedule/62f5611c-a264-11e5-b4d3-005056960017"
+list_mes = """You can use:
+/start - to start the dialog
+/timetable - to see the newest timetable from the BMSTU's site
+/list - to see all commands
+/help - to get som helpful info"""
